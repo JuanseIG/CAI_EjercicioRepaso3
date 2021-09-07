7af2f4d5be87381b2e9aa90cbd7befdca7fd8492
JuanseIG <89666312+JuanseIG@users.noreply.github.com>
2021-09-06 22:32:52 -0300

    Ejercicio 3
    Ejercicio de repaso 3 resuelto

---
+++ CAI_EjercicioRepaso3/CAI_EjercicioRepaso3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAI_EjercicioRepaso3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string palabra = "";
+            string invertida = "";
+            string letra = "";
+            int i = 0;
+
+            Console.WriteLine("ingresar una palabra");
+            palabra = Convert.ToString(Console.ReadLine());
+            i = palabra.Length;
+            for (int x=i-1; x >= 0; x--)
+            {
+                letra = palabra.Substring(x, 1);
+                invertida = invertida + letra; 
+            }
+            if (palabra == invertida)
+            {
+                Console.WriteLine(palabra + " es palíndrome");
+            }
+            else
+            {
+                Console.WriteLine(palabra + " no es palíndrome");
+            }
+
+            Console.ReadKey();
+            
+        }
+    }
+}
